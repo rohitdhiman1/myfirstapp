0a04d7b86c7322657bb22eb006dbf5f3b8a00863
Rohit Dhiman <rohitdhiman0202@gmail.com>
2025-08-26 23:43:34 +0100

    initial commit

---
+++ src/test/java/com/rohitdhiman/demo/myfirstapp/MyfirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohitdhiman.demo.myfirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
